5b5f76d8caa321af726b8f1620801e819f78e736
chqiu6 <chucky-qiu@lambdastudents.com>
2020-10-14 22:02:04 -0400

    setting up todolist

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
   return (
     <div className="App">
-      <p>testing</p>
+      <h1>Todo App</h1>
+      <todoForm/>
+      <todoList/>
     </div>
   );
 }
 
 export default App;
+
+// we want app to have components of todoForm and todoList
